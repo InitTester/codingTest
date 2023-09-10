e7efa8b92762f07533459a306e0600b1b1095702
InitTester <143479869+InitTester@users.noreply.github.com>
2023-09-10 23:16:05 +0900

    [unrated] Title: 문자열로 변환, Time: 0.02 ms, Memory: 70.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181845. 문자열로 변환/문자열로 변환.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(int n) {
+        String answer = "";
+        
+        if(n >=1 && n <= 10000)
+            answer = String.valueOf(n);
+        return answer;
+    }
+}
