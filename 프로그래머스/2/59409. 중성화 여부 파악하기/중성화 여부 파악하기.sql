16ab5bbc915435f465b36563b7932f2d8660667d
InitTester <143479869+InitTester@users.noreply.github.com>
2024-01-21 11:07:19 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -0,0 +1,7 @@
+-- 코드를 입력하세요
+SELECT ANIMAL_ID,
+    NAME,
+    CASE WHEN SEX_UPON_INTAKE LIKE '%Neutered%' THEN 'O' 
+         ELSE CASE WHEN SEX_UPON_INTAKE LIKE '%Spayed%' THEN 'O' ELSE 'X' END END AS '중성화'
+FROM ANIMAL_INS
+
