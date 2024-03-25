639fbee65c5cce768363e0e7c8b085c598271f1d
InitTester <143479869+InitTester@users.noreply.github.com>
2024-03-26 07:51:51 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,16 +1,9 @@
 class Solution {
+
     public String solution(String myString) {
-        String answer = "";
-        
-        for(int i=0; i<myString.length(); i++){
-            if(myString.charAt(i) == 'a' ||myString.charAt(i) == 'A'){
-                answer += (myString.charAt(i) +"").toUpperCase();
-            }else
-            {
-                answer += (myString.charAt(i)+"").toLowerCase();
-            }
-        }
-        
-        return answer;
+        myString = myString.toLowerCase();
+        myString = myString.replace('a', 'A');
+
+        return myString;
     }
 }
