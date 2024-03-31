d786063952c94c43235f41d134b68438992e377a
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-01 08:08:13 +0900

    [level 0] Title: 가위 바위 보, Time: 0.03 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,9 +1,22 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
-        for(int i=0; i<rsp.length(); i++){
-            answer += rsp.substring(i,i+1).equals("2") ? 0 : (rsp.substring(i,i+1).equals("0") ? 5 : 2);
+        char[] cRsp = rsp.toCharArray();
+        StringBuilder answer = new StringBuilder();
+
+        for(int i=0; i < cRsp.length; i++) {
+            switch(cRsp[i]) {
+                case '0' :
+                    answer.append("5");
+                    break;
+                case '2' :
+                    answer.append("0");
+                    break;
+                case '5' :
+                    answer.append("2");
+                    break;
+            }
         }
-        return answer;
+        return answer.toString();
+
     }
 }
