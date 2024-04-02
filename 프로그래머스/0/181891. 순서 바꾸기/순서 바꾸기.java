2f5e422bc29e492f9823ca0ce771cd8a5c51ab59
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-02 20:12:25 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,13 +1,25 @@
+// class Solution {
+//     public int[] solution(int[] num_list, int n) {
+//         int[] answer = new int[num_list.length];
+//         int idx = 0;
+//         for(int i=n; i < num_list.length; i++){
+//             answer[idx++] = num_list[i];
+//         }
+//         for(int i= 0; i<n; i++){
+//             answer[idx++] = num_list[i];
+//         }
+//         return answer;
+//     }
+// }
+
+
+import java.util.Arrays;
+
 class Solution {
-    public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
-        int idx = 0;
-        for(int i=n; i < num_list.length; i++){
-            answer[idx++] = num_list[i];
-        }
-        for(int i= 0; i<n; i++){
-            answer[idx++] = num_list[i];
-        }
-        return answer;
+    public int[] solution(int[] numList, int n) {
+        int[] copy = Arrays.copyOf(numList, numList.length * 2);
+
+        System.arraycopy(numList, 0, copy, numList.length, numList.length);
+        return Arrays.copyOfRange(copy, n, n + numList.length);
     }
 }
