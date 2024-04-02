c7d1be3fa9ac07ece69898171f89ba31f6337eaf
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-02 18:19:34 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.18 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.java
@@ -1,11 +1,29 @@
+// class Solution {
+//     public String solution(String my_string) {
+//         String answer = "";
+//         char[] list = my_string.toCharArray();
+//         for(int i=0; i<list.length; i++){
+//             answer += 65<= list[i] && list[i] <= 90 ? (list[i]+"").toLowerCase() :(list[i]+"").toUpperCase();
+            
+//         }
+//         return answer;
+//     }
+// // }
+
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public String solution(String myString) {
+//         return myString.chars().mapToObj(operand -> String.valueOf((char) (Character.isLowerCase(operand) ? Character.toUpperCase(operand) : Character.toLowerCase(operand)))).collect(Collectors.joining());
+//     }
+// }
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        char[] list = my_string.toCharArray();
-        for(int i=0; i<list.length; i++){
-            answer += 65<= list[i] && list[i] <= 90 ? (list[i]+"").toLowerCase() :(list[i]+"").toUpperCase();
-            
+        StringBuilder answer = new StringBuilder();
+        for(char c : my_string.toCharArray()){
+            answer.append(c >= 'a' && c <= 'z' ? (char)(c - 'a' + 'A') : (char)(c - 'A' + 'a'));
         }
-        return answer;
+        return answer.toString();
     }
 }
