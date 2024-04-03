bdf613361d6a0a6a23ee3330e2eae6fe5a81b957
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-03 21:18:26 +0900

    [level 0] Title: 배열 비교하기, Time: 0.01 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,17 +1,31 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] arr1, int[] arr2) {
+//         int answer = 0;
+//         int sumarr1 = (int)Arrays.stream(arr1).asLongStream().sum();
+//         int sumarr2 = (int)Arrays.stream(arr2).asLongStream().sum();
+        
+//         if(arr1.length!=arr2.length){
+//             answer = arr1.length < arr2.length ? -1 : 1;
+//         }else{
+//             answer = sumarr1 == sumarr2  ? 0 : (sumarr1<sumarr2 ? -1 : 1);
+//         }
+//         return answer;
+//     }
+// }
+
+
+import java.util.stream.IntStream;
 
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        int answer = 0;
-        int sumarr1 = (int)Arrays.stream(arr1).asLongStream().sum();
-        int sumarr2 = (int)Arrays.stream(arr2).asLongStream().sum();
-        
-        if(arr1.length!=arr2.length){
-            answer = arr1.length < arr2.length ? -1 : 1;
-        }else{
-            answer = sumarr1 == sumarr2  ? 0 : (sumarr1<sumarr2 ? -1 : 1);
+        int answer = Integer.compare(arr1.length, arr2.length);
+
+        if(answer == 0) {
+            answer = Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
         }
-        // answer = arr1.length != arr2.length && arr1.length < arr2.length ? arr1.length-arr2.length : ( arr1.length == arr2.length && sumarr1 == sumarr2 ? 0 : 1);
+
         return answer;
     }
 }
