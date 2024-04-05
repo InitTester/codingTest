2cace27207e0e910c984b291310fd09842d1f17d
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-05 10:27:51 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.06 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,16 +1,22 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public String[] solution(String my_string) {
+//         String[] result = my_string.trim().split(" ");
+
+//         ArrayList<String> list = new ArrayList<>();
+
+//         for(String str : result){
+//             if(!str.trim().isEmpty())
+//                 list.add(str);
+//         }
+        
+//         return list.toArray(new String[0]);
+//     }
+// }
 
 class Solution {
     public String[] solution(String my_string) {
-        String[] result = my_string.trim().split(" ");
-
-        ArrayList<String> list = new ArrayList<>();
-
-        for(String str : result){
-            if(!str.trim().isEmpty())
-                list.add(str);
-        }
-        
-        return list.toArray(new String[0]);
+        return my_string.trim().split("[ ]+");
     }
 }
