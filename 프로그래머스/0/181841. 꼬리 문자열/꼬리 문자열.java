67330ffc371c29edc6566319317a89ec15922f08
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-09 10:43:46 +0900

    [level 0] Title: 꼬리 문자열, Time: 2.03 ms, Memory: 67.6 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.java
@@ -1,11 +1,20 @@
+// class Solution {
+//     public String solution(String[] str_list, String ex) {
+//         String answer = "";
+//         for(int i=0; i< str_list.length; i++){
+//             if(str_list[i].indexOf(ex) < 0){
+//                 answer += str_list[i];
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
-    public String solution(String[] str_list, String ex) {
-        String answer = "";
-        for(int i=0; i< str_list.length; i++){
-            if(str_list[i].indexOf(ex) < 0){
-                answer += str_list[i];
-            }
-        }
-        return answer;
+    public String solution(String[] strList, String ex) {
+        return Arrays.stream(strList).filter(s -> !s.contains(ex)).collect(Collectors.joining());
     }
 }
