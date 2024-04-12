d1fd2141c8c948213ac94e1c9f1e40a4c174e211
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-12 14:05:30 +0900

    [level 0] Title: 할 일 목록, Time: 10.93 ms, Memory: 66.7 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,16 +1,28 @@
+// class Solution {
+//     public String[] solution(String[] todo_list, boolean[] finished) {
+        
+//         int len = 0;
+//         for(boolean fal : finished) if(!fal)len++;
+
+//         String[] answer = new String[len];
+//         int idx= 0;
+
+//         for(int i = 0; i<todo_list.length; i++){
+//             if(!finished[i])
+//                 answer[idx++] = todo_list[i];
+//         }
+//         return answer;        
+//     }
+// }
+
+
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        
-        int len = 0;
-        for(boolean fal : finished) if(!fal)len++;
-
-        String[] answer = new String[len];
-        int idx= 0;
-
-        for(int i = 0; i<todo_list.length; i++){
-            if(!finished[i])
-                answer[idx++] = todo_list[i];
+        String str = "";
+        for(int i=0; i<finished.length; i++){
+            str = finished[i]==false ? str+todo_list[i]+"," : str;
         }
-        return answer;        
+
+        return str.split(",");
     }
 }
