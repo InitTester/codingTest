c9f034314d6344326cbbbf95b16fd0d5f5e76031
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-12 14:06:39 +0900

    [level 0] Title: 할 일 목록, Time: 3.67 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -16,13 +16,29 @@
 // }
 
 
-class Solution {
-    public String[] solution(String[] todo_list, boolean[] finished) {
-        String str = "";
-        for(int i=0; i<finished.length; i++){
-            str = finished[i]==false ? str+todo_list[i]+"," : str;
-        }
+// class Solution {
+//     public String[] solution(String[] todo_list, boolean[] finished) {
+//         String str = "";
+//         for(int i=0; i<finished.length; i++){
+//             str = finished[i]==false ? str+todo_list[i]+"," : str;
+//         }
 
-        return str.split(",");
+//         return str.split(",");
+//     }
+// }
+
+
+
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+class Solution {
+    public String[] solution(String[] todoList, boolean[] finished) {
+        return IntStream.range(0, todoList.length)
+            .mapToObj(i -> new AbstractMap.SimpleEntry<>(todoList[i], !finished[i]))
+            .filter(AbstractMap.SimpleEntry::getValue)
+            .map(AbstractMap.SimpleEntry::getKey)
+            .toArray(String[]::new);
     }
 }
