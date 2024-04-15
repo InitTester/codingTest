79d48612e27c871a3ac5ab4e2f553dfa5568719f
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-15 09:29:16 +0900

    [level 0] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.06 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
+++ 프로그래머스/0/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.java
@@ -1,9 +1,30 @@
+// class Solution {
+//     public String solution(String[] my_strings, int[][] parts) {
+//         String answer = "";
+//         for(int i=0; i <my_strings.length; i++){
+//             answer += my_strings[i].substring(parts[i][0],parts[i][1]+1);
+//         }
+//         return answer;
+//     }
+// }
+
+// import java.util.stream.*;
+
+// class Solution {
+//     public String solution(String[] myStrings, int[][] parts) {
+//         return IntStream.range(0, myStrings.length).mapToObj(i -> myStrings[i].substring(parts[i][0], parts[i][1] + 1)).collect(Collectors.joining());
+//     }
+// }
+
+
 class Solution {
     public String solution(String[] my_strings, int[][] parts) {
-        String answer = "";
-        for(int i=0; i <my_strings.length; i++){
-            answer += my_strings[i].substring(parts[i][0],parts[i][1]+1);
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < my_strings.length; i++) {
+            sb.append(my_strings[i].substring(parts[i][0], parts[i][1] + 1));
         }
-        return answer;
+
+        return sb.toString();
     }
 }
