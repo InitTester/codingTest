a710654df604452a56fbb65a2e3d48ef4262720a
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-15 09:42:50 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/120837. 개미 군단/개미 군단.java
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.java
@@ -1,15 +1,23 @@
+// class Solution {
+//     public int solution(int hp) {
+//         int answer = 0;
+//         int[] list = {5,3,1};
+//         int tmp = hp;
+        
+//         for(int attack : list) {
+//             if(tmp >0){
+//                 answer += tmp/attack;
+//                 tmp -= attack * (tmp/attack) ;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+
+
 class Solution {
     public int solution(int hp) {
-        int answer = 0;
-        int[] list = {5,3,1};
-        int tmp = hp;
-        
-        for(int attack : list) {
-            if(tmp >0){
-                answer += tmp/attack;
-                tmp -= attack * (tmp/attack) ;
-            }
-        }
-        return answer;
+        return hp / 5 + (hp % 5 / 3) + hp % 5 % 3;
     }
 }
