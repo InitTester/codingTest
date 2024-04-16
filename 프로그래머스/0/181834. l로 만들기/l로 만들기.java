d82feb9bd33605edc3dbfa82e7d662c8f0fee079
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-17 07:42:28 +0900

    [level 0] Title: l로 만들기, Time: 12.83 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,9 +1,18 @@
-class Solution {
-    public String solution(String myString) {
-        String answer = "";
-    for(char c : myString.toCharArray()){
-        answer += c <='l' ? "l" : c;            
-    }
-        return answer;
+// class Solution {
+//     public String solution(String myString) {
+//         StringBuilder answer = new StringBuilder();
+        
+//         for(char c : myString.toCharArray()){
+//             answer.append(c <'l' ? 'l' : c);            
+//         }
+        
+//         return answer.toString();
+//     }
+// }
+
+class Solution{
+    public String solution(String myString){
+        
+        return myString.replaceAll("[^l-z]","l");
     }
 }
