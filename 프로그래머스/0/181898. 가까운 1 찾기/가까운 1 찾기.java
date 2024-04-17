20ff540a5d3118085002b82d01c9627751fe2e11
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-17 20:11:13 +0900

    [level 0] Title: 가까운 1 찾기, Time: 1.94 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -1,11 +1,20 @@
+// class Solution {
+//     public int solution(int[] arr, int idx) {
+//         int answer = -1;
+        
+//         for(int i=idx; i<arr.length; i++){
+//             if(1 <= arr[i]) {answer = i; break;}
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+import java.util.stream.IntStream;
+
 class Solution {
     public int solution(int[] arr, int idx) {
-        int answer = -1;
-        
-        for(int i=idx; i<arr.length; i++){
-            if(1 <= arr[i]) {answer = i; break;}
-        }
-        
-        return answer;
+        return IntStream.range(idx, arr.length).filter(i -> arr[i] == 1).findFirst().orElse(-1);
     }
 }
