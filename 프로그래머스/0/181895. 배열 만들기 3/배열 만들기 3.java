511d760d33f054080351cdb619750b4a50c86761
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-30 13:26:19 +0900

    [level 0] Title: 배열 만들기 3, Time: 4.62 ms, Memory: 84.1 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,16 +1,26 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr, int[][] intervals) {
+        
+//         ArrayList<Integer> list = new ArrayList<>();
+        
+//         for(int i=0; i< intervals.length; i++){
+//             for(int j=intervals[i][0]; j<=intervals[i][1]; j++){
+//                  list.add(arr[j]);
+//             }
+//         }
+        
+//         return list.stream().mapToInt(Integer::intValue).toArray();
+//     }
+// }
+
+
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[][] intervals) {
-        
-        ArrayList<Integer> list = new ArrayList<>();
-        
-        for(int i=0; i< intervals.length; i++){
-            for(int j=intervals[i][0]; j<=intervals[i][1]; j++){
-                 list.add(arr[j]);
-            }
-        }
-        
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return Arrays.stream(intervals).flatMapToInt(ints -> Arrays.stream(Arrays.copyOfRange(arr, ints[0], ints[1] + 1))).toArray();
     }
 }
