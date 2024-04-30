9d0cd3452854eba58d1be8c2384e38f4fdb78e50
InitTester <143479869+InitTester@users.noreply.github.com>
2024-04-30 13:32:43 +0900

    [level 0] Title: 배열 만들기 3, Time: 0.03 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.java
@@ -1,19 +1,18 @@
+// class Solution {
+//     public int[] solution(int[] arr, int[][] intervals) {
+//         int len = (intervals[0][1]-intervals[0][0]) + (intervals[1][1] - intervals[1][0] +2);
 
-class Solution {
-    public int[] solution(int[] arr, int[][] intervals) {
-        int len = (intervals[0][1]-intervals[0][0]) + (intervals[1][1] - intervals[1][0] +2);
+//         int[] answer = new int[len];
+//         int idx =0;
 
-        int[] answer = new int[len];
-        int idx =0;
-
-        for(int i=0; i< intervals.length; i++){
-            for(int j=intervals[i][0]; j<=intervals[i][1]; j++){
-                answer[idx++] = arr[j];
-            }
-        }
-        return answer;
-    }
-}
+//         for(int i=0; i< intervals.length; i++){
+//             for(int j=intervals[i][0]; j<=intervals[i][1]; j++){
+//                 answer[idx++] = arr[j];
+//             }
+//         }
+//         return answer;
+//     }
+// }
 
 
 // import java.util.*;
@@ -42,3 +41,21 @@ class Solution {
 //         return Arrays.stream(intervals).flatMapToInt(ints -> Arrays.stream(Arrays.copyOfRange(arr, ints[0], ints[1] + 1))).toArray();
 //     }
 // }
+
+
+
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int x1 = intervals[0][0];
+        int y1 = intervals[0][1];
+        int x2 = intervals[1][0];
+        int y2 = intervals[1][1];
+        int[] answer = new int[(y1-x1+1)+(y2-x2+1)];
+
+        System.arraycopy(arr, x1, answer, 0, y1-x1+1);
+        System.arraycopy(arr, x2, answer, y1-x1+1, y2-x2+1);
+
+        return answer;
+    }
+}
