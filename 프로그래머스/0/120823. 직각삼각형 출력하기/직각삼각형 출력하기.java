5fd0c6d224436dc56da0fe0832f211b926cc5e40
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-08 21:59:01 +0900

    [level 0] Title: 직각삼각형 출력하기, Time: 143.55 ms, Memory: 63.5 MB -BaekjoonHub

--- 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
+++ 프로그래머스/0/120823. 직각삼각형 출력하기/직각삼각형 출력하기.java
@@ -1,15 +1,41 @@
-import java.util.Scanner;
+// import java.util.Scanner;
+
+// public class Solution {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         int n = sc.nextInt();
+
+//         // for(int i=0; i < n; i++){
+//         //     for(int j=0; j<i+1; j++){
+//         //         System.out.print("*");
+//         //     }
+//         //     System.out.println();
+//         // }
+        
+//         for(int i=1; i<=n; i++){
+//             System.out.println("*".repeat(i));
+//         }
+//     }
+// }
+
+
+
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Solution {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        for(int i=0; i < n; i++){
-            for(int j=0; j<i+1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+        int n = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i <= n; i++){
+            sb.append("*".repeat(i)).append("\n");
         }
+        System.out.print(sb.toString());
     }
 }
