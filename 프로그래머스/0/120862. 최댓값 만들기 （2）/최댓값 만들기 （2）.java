6841d549bfc23668d135eba1ae9d863bb4316d93
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-14 08:36:19 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.37 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,23 +1,35 @@
+// import java.util.*;
+
+// class Solution {
+//     public int solution(int[] numbers) {
+//         int answer = 0;
+//         int max = numbers[0] * numbers[1];
+
+//         for(int i =0; i < numbers.length; i++) {
+//             for(int j=0; j < numbers.length; j++){
+//                 int now = numbers[i]*numbers[j];
+
+//                 if(i!=j) {
+//                     if (max <= now) {
+//                         max = now;
+//                         answer = max;
+//                     }
+//                 }
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 0;
-        int max = numbers[0] * numbers[1];
-
-        for(int i =0; i < numbers.length; i++) {
-            for(int j=0; j < numbers.length; j++){
-                int now = numbers[i]*numbers[j];
-
-                if(i!=j) {
-                    if (max <= now) {
-                        max = now;
-                        answer = max;
-                    }
-                }
-            }
-        }
-        
-        return answer;
+        int len = numbers.length;
+        Arrays.sort(numbers);
+        return Math.max(numbers[0] * numbers[1], numbers[len - 2] * numbers[len - 1]);
     }
 }
