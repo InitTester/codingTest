c0c5e7faca364b4c0be905fcd2594c9ab370fb52
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-15 09:24:00 +0900

    [level 0] Title: qr code, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -2,11 +2,27 @@ import java.util.*;
 
 class Solution {
     public String solution(int q, int r, String code) {
-        StringBuilder answer = new StringBuilder();
-        for(int i=0 ; i <code.toCharArray().length; i++){
-            if(i%q ==r)
-                answer.append(code.charAt(i));
-        }
-        return answer.toString();
+//         String answer = "";
+
+//         for(int i=0 ; i <code.toCharArray().length; i++){
+
+//             if(i%q ==r)
+//                 answer += code.charAt(i);
+//         }
+//         return answer;
+//         StringBuilder answer = new StringBuilder();
+        
+//         for(int i=0 ; i <code.toCharArray().length; i++){
+//             if(i%q ==r)
+//                 answer.append(code.charAt(i));
+//         }
+        
+//         return answer.toString();
+       StringBuilder sb = new StringBuilder();
+
+        for (int i = r; i < code.length(); i += q)
+            sb.append(code.charAt(i));
+
+        return sb.toString();
     }
 }
