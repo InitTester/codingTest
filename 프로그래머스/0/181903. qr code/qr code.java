741dda400405ebd678b34474fcac9e94b4bc68b8
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-15 09:21:23 +0900

    [level 0] Title: qr code, Time: 5.73 ms, Memory: 67.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int q, int r, String code) {
+        StringBuilder answer = new StringBuilder();
+        for(int i=0 ; i <code.toCharArray().length; i++){
+            if(i%q ==r)
+                answer.append(code.charAt(i));
+        }
+        return answer.toString();
+    }
+}
