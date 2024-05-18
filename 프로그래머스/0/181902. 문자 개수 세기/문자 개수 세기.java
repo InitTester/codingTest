6d120c0eed8cfc919f1bb90a98755dcc85f94f86
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-18 11:34:01 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.03 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -1,13 +1,28 @@
+// class Solution {
+//     public int[] solution(String my_string) {
+//         int[] answer = new int [52];
+
+//         for(char c : my_string.toCharArray()){
+//             if (96 < (int) c) {
+//                 answer[(int)c-71] += 1;
+//             } else {
+//                 answer[(int) c - 65] += 1;
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+
 class Solution {
     public int[] solution(String my_string) {
-        int[] answer = new int [52];
-
-        for(char c : my_string.toCharArray()){
-            if (96 < (int) c) {
-                answer[(int)c-71] += 1;
-            } else {
-                answer[(int) c - 65] += 1;
-            }
+        int[] answer = new int[52];
+        for(int i = 0; i < my_string.length(); i++){
+            char c = my_string.charAt(i);
+            if(c >= 'a')
+                answer[c - 'a' + 26]++;
+            else
+                answer[c - 'A']++;
         }
         return answer;
     }
