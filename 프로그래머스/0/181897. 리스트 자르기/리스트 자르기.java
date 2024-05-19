7d9445ac8f19a208e38aac46a0bf2088d8710c9d
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-19 20:42:31 +0900

    [level 0] Title: 리스트 자르기, Time: 0.01 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -1,18 +1,31 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int n, int[] slicer, int[] num_list) {
+
+//         int length = n== 2 ? num_list.length : slicer[1]+1;
+//         int start = n== 1 ? 0 : slicer[0];
+//         int slice = n == 4 ? slicer[2] : 1;
+        
+//         List<Integer> list = new ArrayList<>();
+        
+//         for(int i =start; i < length; i = i + slice){
+//             list.add(num_list[i]);
+//         }
+        
+//         return list.stream().mapToInt(i->i).toArray();
+//     }
+// }
 
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-
-        int length = n== 2 ? num_list.length : slicer[1]+1;
-        int start = n== 1 ? 0 : slicer[0];
-        int slice = n == 4 ? slicer[2] : 1;
-        
-        List<Integer> list = new ArrayList<>();
-        
-        for(int i =start; i < length; i = i + slice){
-            list.add(num_list[i]);
+        int start = n == 1 ? 0 : slicer[0];
+        int end = n == 2 ? num_list.length - 1 : slicer[1];
+        int step = n == 4 ? slicer[2] : 1;
+        int[] answer = new int[(end - start + step) / step];
+        for (int i = start, j = 0; i <= end; i += step) {
+            answer[j++] = num_list[i];
         }
-        
-        return list.stream().mapToInt(i->i).toArray();
+        return answer;
     }
 }
