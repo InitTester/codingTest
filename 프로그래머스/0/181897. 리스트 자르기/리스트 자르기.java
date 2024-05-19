b239602d71a738492e0ef4416a292afb523584d5
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-19 20:44:47 +0900

    [level 0] Title: 리스트 자르기, Time: 3.82 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -17,15 +17,43 @@
 //     }
 // }
 
+// class Solution {
+//     public int[] solution(int n, int[] slicer, int[] num_list) {
+//         int start = n == 1 ? 0 : slicer[0];
+//         int end = n == 2 ? num_list.length - 1 : slicer[1];
+//         int step = n == 4 ? slicer[2] : 1;
+//         int[] answer = new int[(end - start + step) / step];
+//         for (int i = start, j = 0; i <= end; i += step) {
+//             answer[j++] = num_list[i];
+//         }
+//         return answer;
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 class Solution {
     public int[] solution(int n, int[] slicer, int[] num_list) {
-        int start = n == 1 ? 0 : slicer[0];
-        int end = n == 2 ? num_list.length - 1 : slicer[1];
-        int step = n == 4 ? slicer[2] : 1;
-        int[] answer = new int[(end - start + step) / step];
-        for (int i = start, j = 0; i <= end; i += step) {
-            answer[j++] = num_list[i];
+
+        int[] answer = null;
+        int a = slicer[0], b = slicer[1], c = slicer[2];
+
+        switch (n) {
+        case 1:
+            answer = Arrays.copyOf(num_list, b + 1);
+            break;
+        case 2:
+            answer = Arrays.copyOfRange(num_list, a, num_list.length);
+            break;
+        case 3:
+            answer = Arrays.copyOfRange(num_list, a, b + 1);
+            break;
+        case 4:
+            answer = IntStream.range(0, (b - a) / c + 1).map(i -> num_list[a + i * c]).toArray();
+            break;
         }
+
         return answer;
     }
 }
