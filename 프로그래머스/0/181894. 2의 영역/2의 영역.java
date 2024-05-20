b07e60356e10f55f6c2324e1609e47ca95d30903
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-20 10:17:57 +0900

    [level 0] Title: 2의 영역, Time: 8.61 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/0/181894. 2의 영역/2의 영역.java
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -1,30 +1,40 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr) {
+
+//         int start = -1;
+//         int end = -1;
+        
+//         for(int i=0 ; i< arr.length; i++){
+//             if(arr[i]==2){
+//                 if(start==-1){
+//                     start = i;
+//                 }
+//                 end = i;
+//             }    
+//         }
+        
+//         int[] answer = new int[start<0? 1: end-start+1];
+        
+//         if(start<0){
+//             answer[0] = -1;
+//         }else{
+//             for(int i=start, j=0; i <= end; i++){
+//                 answer[j++] = arr[i];
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] arr) {
-
-        int start = -1;
-        int end = -1;
-        
-        for(int i=0 ; i< arr.length; i++){
-            if(arr[i]==2){
-                if(start==-1){
-                    start = i;
-                }
-                end = i;
-            }    
-        }
-        
-        int[] answer = new int[start<0? 1: end-start+1];
-        
-        if(start<0){
-            answer[0] = -1;
-        }else{
-            for(int i=start, j=0; i <= end; i++){
-                answer[j++] = arr[i];
-            }
-        }
-        
-        return answer;
+        int[] idx = IntStream.range(0, arr.length).filter(i -> arr[i] == 2).toArray();
+        if (idx.length == 0) return new int[]{-1};
+        return IntStream.rangeClosed(idx[0], idx[idx.length - 1]).map(i -> arr[i]).toArray();
     }
 }
