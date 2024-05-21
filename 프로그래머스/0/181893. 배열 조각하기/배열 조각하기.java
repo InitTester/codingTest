f78def9309512a01da6c8e34299ebef3d5053130
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-21 09:17:06 +0900

    [level 0] Title: 배열 조각하기, Time: 0.03 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -1,21 +1,38 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr, int[] query) {
+//         int[] answer = {};        
+//         int[] tmp = arr;
+        
+//         for(int i=0; i < query.length; i++){
+//             if(i%2==0){
+//                 tmp = Arrays.copyOfRange(tmp,0,query[i]+1);        
+//             }else{                
+//                 tmp = Arrays.copyOfRange(tmp,query[i],tmp.length);        
+//             }
+//         }
+        
+//         answer = tmp;
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        int[] answer = {};
-        
-        int[] tmp = arr;
-        
-        for(int i=0; i < query.length; i++){
-            if(i%2==0){
-                tmp = Arrays.copyOfRange(tmp,0,query[i]+1);        
-            }else{                
-                tmp = Arrays.copyOfRange(tmp,query[i],tmp.length);        
+        int start = 0;
+        int end = arr.length - 1;
+        for (int i = 0; i < query.length; i++) {
+            if (i % 2 == 0) {
+                end = start + query[i] - 1;
+            } else {
+                start += query[i];
             }
         }
-        
-        answer = tmp;
-        
-        return answer;
+
+        return Arrays.copyOfRange(arr, start, end + 2);
     }
 }
