6e137da8762f0e48eee30b22cdc760091a9a9d12
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-21 09:18:14 +0900

    [level 0] Title: 배열 조각하기, Time: 1.56 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
+++ 프로그래머스/0/181893. 배열 조각하기/배열 조각하기.java
@@ -19,20 +19,34 @@
 //     }
 // }
 
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr, int[] query) {
+//         int start = 0;
+//         int end = arr.length - 1;
+//         for (int i = 0; i < query.length; i++) {
+//             if (i % 2 == 0) {
+//                 end = start + query[i] - 1;
+//             } else {
+//                 start += query[i];
+//             }
+//         }
+
+//         return Arrays.copyOfRange(arr, start, end + 2);
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] arr, int[] query) {
-        int start = 0;
-        int end = arr.length - 1;
-        for (int i = 0; i < query.length; i++) {
-            if (i % 2 == 0) {
-                end = start + query[i] - 1;
-            } else {
-                start += query[i];
-            }
-        }
-
-        return Arrays.copyOfRange(arr, start, end + 2);
+        return IntStream.range(0, query.length)
+                .boxed()
+                .reduce(arr, (current, next) -> next % 2 == 0 ?
+                                Arrays.copyOfRange(current, 0, query[next] + 1) :
+                                Arrays.copyOfRange(current, query[next], current.length),
+                        (a, b) -> a);
     }
 }
