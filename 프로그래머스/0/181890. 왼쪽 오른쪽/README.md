ff0a2e7acb3c1ff03a2a2f719b189f3fc0a00f34
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-22 12:59:11 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 1.84 ms, Memory: 83.2 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890#) 
 
 ### 성능 요약
 
-메모리: 78 MB, 시간: 0.04 ms
+메모리: 83.2 MB, 시간: 1.84 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 22일 12:53:57
+2024년 05월 22일 12:59:07
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -1,18 +1,37 @@
-import java.util.*;
+// import java.util.*;
+
+// class Solution {
+//     public String[] solution(String[] str_list) {
+//         String[] answer = {};
+//         for(int i=0; i < str_list.length; i++){
+//             if(str_list[i].equals("l")){
+//                 answer = Arrays.copyOfRange(str_list,0,i);
+//                 break;
+//             }else if(str_list[i].equals("r")){
+//                 answer = Arrays.copyOfRange(str_list,i+1,str_list.length);
+//                 break;
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        String[] answer = {};
-        for(int i=0; i < str_list.length; i++){
-            if(str_list[i].equals("l")){
-                answer = Arrays.copyOfRange(str_list,0,i);
-                break;
-            }else if(str_list[i].equals("r")){
-                answer = Arrays.copyOfRange(str_list,i+1,str_list.length);
-                break;
-            }
-        }
-        
-        return answer;
+        return IntStream.range(0, str_list.length)
+                .boxed()
+                .filter(i -> str_list[i].equals("l") || str_list[i].equals("r"))
+                .findFirst()
+                .map(i -> {
+                    if (str_list[i].equals("l")) {
+                        return Arrays.copyOfRange(str_list, 0, i);
+                    }
+                    return Arrays.copyOfRange(str_list, i + 1, str_list.length);
+                })
+                .orElseGet(() -> new String[]{});
     }
 }
