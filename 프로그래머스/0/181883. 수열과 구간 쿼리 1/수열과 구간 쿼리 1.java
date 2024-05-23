9c4c551b9fd69e535ec1e35c4fc982c90b15cc8a
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-23 09:24:39 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 1.29 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.java
@@ -1,18 +1,30 @@
+// class Solution {
+//     public int[] solution(int[] arr, int[][] queries) {
+        
+//         int[] answer = arr;
+        
+//         for(int i=0; i < queries.length; i++){
+//             int s = queries[i][0];
+//             int e = queries[i][1];
+            
+//             if(s <= e){
+//                 for(int j=s; j<=e; j++ ){
+//                     answer[j]++;
+//                 }
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        
-        int[] answer = arr;
-        
-        for(int i=0; i < queries.length; i++){
-            int s = queries[i][0];
-            int e = queries[i][1];
-            
-            if(s <= e){
-                for(int j=s; j<=e; j++ ){
-                    answer[j]++;
-                }
-            }
+
+        for (int[] query : queries) {
+            int s = query[0], e = query[1];
+            for (int i = s; i <= e; i++)
+                arr[i]++;
         }
-        return answer;
+        return arr;
     }
 }
