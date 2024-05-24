e32ea09f072281b1a2456bd14766febfb1329006
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-24 09:38:07 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 2, Time: 1.25 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
+++ 프로그래머스/0/181881. 조건에 맞게 수열 변환하기 2/조건에 맞게 수열 변환하기 2.java
@@ -1,35 +1,64 @@
+// class Solution {
+//     public int solution(int[] arr) {
+//         int answer=0;
+//         int[] tmp = new int[arr.length];
+
+//         for(int i=0; i < arr.length; i++) {
+
+//             int idx =0, cnt =0;
+
+//             for (int k : arr) {tmp[idx++] = k;}
+
+//             for (int j = 0; j < arr.length; j++) {
+//                 if (arr[j] >= 50 && arr[j] % 2 == 0) {
+//                     arr[j] = arr[j] / 2;
+//                 } else if (arr[j] <= 50 && arr[j] % 2 == 1) {
+//                     arr[j] = arr[j] * 2 + 1;
+//                 }
+//             }
+
+//             idx=0;
+            
+//             for (int s : tmp) {
+//                 if (s == arr[idx++]) {
+//                     cnt++;
+//                 }
+//             }
+
+//             if (cnt == arr.length) {
+//                 answer = i;
+//                 break;
+//             }
+//         }
+//       return answer;
+//     }
+// }
+
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] arr) {
-        int answer=0;
-        int[] tmp = new int[arr.length];
+        return Arrays.stream(arr).map(i -> cntAction(i)).max().getAsInt();
+    }
 
-        for(int i=0; i < arr.length; i++) {
+    private int cntAction(int n) {
 
-            int idx =0, cnt =0;
+        int cnt = 0;
+        boolean flag = true;
 
-            for (int k : arr) {tmp[idx++] = k;}
+        while (flag) {
 
-            for (int j = 0; j < arr.length; j++) {
-                if (arr[j] >= 50 && arr[j] % 2 == 0) {
-                    arr[j] = arr[j] / 2;
-                } else if (arr[j] <= 50 && arr[j] % 2 == 1) {
-                    arr[j] = arr[j] * 2 + 1;
-                }
-            }
-
-            idx=0;
-            
-            for (int s : tmp) {
-                if (s == arr[idx++]) {
-                    cnt++;
-                }
-            }
-
-            if (cnt == arr.length) {
-                answer = i;
-                break;
+            if (n % 2 == 0 && 50 <= n) {
+                n /= 2;
+                cnt++;
+            } else if (n % 2 == 1 && n < 50) {
+                n *= 2;
+                n += 1;
+                cnt++;
+            } else {
+                flag = false;
             }
         }
-      return answer;
+        return cnt;
     }
 }
