11f1d402e076ce930bc532c2e1e3b1fc99695436
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-27 10:30:51 +0900

    [level 0] Title: 세 개의 구분자, Time: 52.03 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -2,68 +2,41 @@
 
 // class Solution {
 //     public String[] solution(String myStr) {
-//         String[] answer = {};
-//         List<String> list = new ArrayList();
+//         List<String> list = new ArrayList<>();
 //         StringBuilder tmp = new StringBuilder();
-//         int cnt = 0;
+//         boolean foundNonABC = false;
 
-//         for(int i=0; i < myStr.toCharArray().length; i++){
+//         for (int i = 0; i < myStr.length(); i++) {
+//             char currentChar = myStr.charAt(i);
 
-//             if(!(myStr.charAt(i)=='a' || myStr.charAt(i)=='b' || myStr.charAt(i)=='c')) {
-//                 tmp.append(myStr.charAt(i));
-//                 cnt++;
-//             }else{
-//                 if(tmp.length()>0) {
+//             if (currentChar != 'a' && currentChar != 'b' && currentChar != 'c') {
+//                 tmp.append(currentChar);
+//                 foundNonABC = true;
+//             } else {
+//                 if (tmp.length() > 0) {
 //                     list.add(tmp.toString());
-//                     tmp = new StringBuilder();
+//                     tmp.setLength(0);  
 //                 }
 //             }
-
-//             if(i == myStr.toCharArray().length-1){
-//                 list.add(tmp.toString());
-//             }
 //         }
 
-//         if(cnt==0){
-//             answer = new String[]{"EMPTY"};
-//         }else{
-//             answer = list.toArray(new String[list.size()]);
+//         if (tmp.length() > 0) {
+//             list.add(tmp.toString());
 //         }
-               
-//         return answer;
+
+//         if (!foundNonABC) {
+//             return new String[]{"EMPTY"};
+//         }
+
+//         return list.toArray(new String[0]);
 //     }
 // }
 
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
     public String[] solution(String myStr) {
-        List<String> list = new ArrayList<>();
-        StringBuilder tmp = new StringBuilder();
-        boolean foundNonABC = false;
-
-        for (int i = 0; i < myStr.length(); i++) {
-            char currentChar = myStr.charAt(i);
-
-            if (currentChar != 'a' && currentChar != 'b' && currentChar != 'c') {
-                tmp.append(currentChar);
-                foundNonABC = true;
-            } else {
-                if (tmp.length() > 0) {
-                    list.add(tmp.toString());
-                    tmp.setLength(0);  
-                }
-            }
-        }
-
-        if (tmp.length() > 0) {
-            list.add(tmp.toString());
-        }
-
-        if (!foundNonABC) {
-            return new String[]{"EMPTY"};
-        }
-
-        return list.toArray(new String[0]);
+        String[] arr = Arrays.stream(myStr.split("[abc]+")).filter(str -> !str.isEmpty()).toArray(String[]::new);
+        return arr.length == 0 ? new String[] { "EMPTY" } : arr;
     }
 }
