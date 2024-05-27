cbada91125d3e7b82712470802cf043c6d13192c
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-27 10:34:36 +0900

    [level 0] Title: 세 개의 구분자, Time: 73.23 ms, Memory: 99.6 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -41,21 +41,57 @@
 //     }
 // }
 
-import java.util.StringTokenizer;
+// import java.util.StringTokenizer;
+// class Solution {
+//     public String[] solution(String myStr) {
+//         String[] answer = {};
+//         StringTokenizer st = new StringTokenizer(myStr,"abc");
+//         if(st.countTokens() != 0){
+//             answer = new String[st.countTokens()];
+//             for(int i=0; i<answer.length; i++){
+//                 answer[i] = st.nextToken();
+//             }
+//         }else{
+//             answer = new String[1];
+//             answer[0] = "EMPTY";
+//         }
+
+//         return answer;
+//     }
+// }
+
+// import java.util.*;
+
+// class Solution {
+//     public String[] solution(String myStr) {
+//         // 구분자로 'a', 'b', 'c'를 설정하고 StringTokenizer 사용
+//         StringTokenizer st = new StringTokenizer(myStr, "abc");
+//         List<String> list = new ArrayList<>();
+        
+//         while (st.hasMoreTokens()) {
+//             String token = st.nextToken();
+//             if (!token.isEmpty()) {
+//                 list.add(token);
+//             }
+//         }
+        
+//         // list가 비어있다면 "EMPTY" 추가
+//         if (list.isEmpty()) {
+//             return new String[]{"EMPTY"};
+//         }
+        
+//         return list.toArray(new String[0]);
+//     }
+// }
+
+
+import java.util.Arrays;
 class Solution {
     public String[] solution(String myStr) {
-        String[] answer = {};
-        StringTokenizer st = new StringTokenizer(myStr,"abc");
-        if(st.countTokens() != 0){
-            answer = new String[st.countTokens()];
-            for(int i=0; i<answer.length; i++){
-                answer[i] = st.nextToken();
-            }
-        }else{
-            answer = new String[1];
-            answer[0] = "EMPTY";
-        }
+        myStr = myStr.replaceAll("[a|b|c]+", ",");
+        myStr = myStr.charAt(0) == ',' ? myStr.substring(1) : myStr;
+        myStr = myStr.equals("") ? "EMPTY" : myStr;
 
-        return answer;
+        return myStr.split("[,]");
     }
 }
