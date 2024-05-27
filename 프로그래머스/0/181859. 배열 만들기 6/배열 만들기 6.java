04a3b27dabe6c4068750b37c720b43654c739834
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-28 08:28:24 +0900

    [level 0] Title: 배열 만들기 6, Time: 18.74 ms, Memory: 89.8 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.java
@@ -1,25 +1,42 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] solution(int[] arr) {
+//         List<Integer> stk = new ArrayList();
+        
+//         for(int i =0; i< arr.length; i++){
+//             int size = stk.size()-1;
+            
+//             if(stk.isEmpty()){
+//                 stk.add(arr[i]);
+//             } else if(!stk.isEmpty() && stk.get(size)==arr[i]){
+//                 stk.remove(size);
+//             } else {
+//                 stk.add(arr[i]);
+//             }            
+//         }
+        
+//         if(stk.isEmpty()){
+//             return new int[]{-1};
+//         }else{
+//             return stk.stream().mapToInt(i -> i).toArray();
+//         }
+//     }
+// }
 import java.util.*;
 
 class Solution {
-    public int[] solution(int[] arr) {
-        List<Integer> stk = new ArrayList();
-        
-        for(int i =0; i< arr.length; i++){
-            int size = stk.size()-1;
-            
-            if(stk.isEmpty()){
-                stk.add(arr[i]);
-            } else if(!stk.isEmpty() && stk.get(size)==arr[i]){
-                stk.remove(size);
-            } else {
-                stk.add(arr[i]);
-            }            
-        }
-        
-        if(stk.isEmpty()){
-            return new int[]{-1};
-        }else{
-            return stk.stream().mapToInt(i -> i).toArray();
-        }
-    }
+   public int[] solution(int[] arr){
+       Stack<Integer> stk = new Stack();
+       
+       for(int i : arr){
+           if(!stk.isEmpty() && i==stk.peek()){
+               stk.pop();
+           }else{
+               stk.push(i);
+           }
+       }
+       return stk.isEmpty() ? new int[]{-1} : stk.stream().mapToInt(i->i).toArray();
+       
+   } 
 }
