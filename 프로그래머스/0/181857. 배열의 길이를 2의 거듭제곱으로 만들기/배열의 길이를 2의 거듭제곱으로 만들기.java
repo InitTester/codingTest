8b48cf4275d759a65f7ddef466a42051f14ac2e6
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-29 09:34:41 +0900

    [level 0] Title: 배열의 길이를 2의 거듭제곱으로 만들기, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
+++ 프로그래머스/0/181857. 배열의 길이를 2의 거듭제곱으로 만들기/배열의 길이를 2의 거듭제곱으로 만들기.java
@@ -1,34 +1,49 @@
+// class Solution {
+//     public int[] solution(int[] arr) {
+        
+//         int len = arr.length;
+//         int listLen =0;
+
+//         // 2의 거듭 제곱인지 아닌지
+//         for(int i=0; i <11; i++){
+//             if(len <= Math.pow(2,i)){
+//                 listLen = (int)Math.pow(2,i);
+//                 break;
+//             }
+//         }
+
+//         int[] answer = new int[listLen];
+
+//         // 2의 거듭 제곱이면 ?
+//         if(len==listLen){
+//             for(int i=0; i < arr.length;i ++){
+//                 answer[i] = arr[i];
+//             }
+//         }else{
+//             for(int i=0; i < listLen; i++){
+//                 if(i<len){
+//                     answer[i] = arr[i];
+//                 }else{
+//                     answer[i] = 0;
+//                 }
+//             }
+//         }
+        
+//         return answer;
+//     }
+// }
+
+
+import java.util.*;
+
 class Solution {
     public int[] solution(int[] arr) {
-        
-        int len = arr.length;
-        int listLen =0;
+        int length = 1;
 
-        // 2의 거듭 제곱인지 아닌지
-        for(int i=0; i <11; i++){
-            if(len <= Math.pow(2,i)){
-                listLen = (int)Math.pow(2,i);
-                break;
-            }
+        while (length < arr.length) {
+            length *= 2;
         }
 
-        int[] answer = new int[listLen];
-
-        // 2의 거듭 제곱이면 ?
-        if(len==listLen){
-            for(int i=0; i < arr.length;i ++){
-                answer[i] = arr[i];
-            }
-        }else{
-            for(int i=0; i < listLen; i++){
-                if(i<len){
-                    answer[i] = arr[i];
-                }else{
-                    answer[i] = 0;
-                }
-            }
-        }
-        
-        return answer;
+        return Arrays.copyOf(arr, length);
     }
 }
