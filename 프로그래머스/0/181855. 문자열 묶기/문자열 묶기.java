acbe3271e17228daa5e29ab259b4bc3de7865cd0
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-30 11:40:30 +0900

    [level 0] Title: 문자열 묶기, Time: 16.76 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -1,14 +1,23 @@
+// import java.util.*;
+
+// class Solution {
+//     public int solution(String[] strArr) {
+//         int answer = Integer.MIN_VALUE;
+        
+//         HashMap<Integer, Integer> list = new HashMap();
+//         for(String s : strArr){ list.put(s.length(), list.get(s.length())==null ? 1 : list.get(s.length())+1);}
+        
+//         for(int i : list.values()){if(i>answer) { answer = i;}}
+        
+//         return answer;
+//     }
+// }
+
 import java.util.*;
+import java.util.stream.Collectors;
 
 class Solution {
     public int solution(String[] strArr) {
-        int answer = Integer.MIN_VALUE;
-        
-        HashMap<Integer, Integer> list = new HashMap();
-        for(String s : strArr){ list.put(s.length(), list.get(s.length())==null ? 1 : list.get(s.length())+1);}
-        
-        for(int i : list.values()){if(i>answer) { answer = i;}}
-        
-        return answer;
+        return Arrays.stream(strArr).collect(Collectors.groupingBy(String::length)).values().stream().max(Comparator.comparingInt(List::size)).orElse(Collections.emptyList()).size();
     }
 }
