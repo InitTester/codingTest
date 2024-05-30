f1eef15f9200741a3785523a0ab3c236dd01d660
InitTester <143479869+InitTester@users.noreply.github.com>
2024-05-30 11:48:30 +0900

    [level 0] Title: 문자열 묶기, Time: 19.46 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -13,11 +13,44 @@
 //     }
 // }
 
+// import java.util.*;
+// import java.util.stream.Collectors;
+
+// class Solution {
+//     public int solution(String[] strArr) {
+//         return Arrays.stream(strArr).collect(Collectors.groupingBy(String::length)).values().stream().max(Comparator.comparingInt(List::size)).orElse(Collections.emptyList()).size();
+//     }
+// }
+
+
+
 import java.util.*;
-import java.util.stream.Collectors;
 
 class Solution {
     public int solution(String[] strArr) {
-        return Arrays.stream(strArr).collect(Collectors.groupingBy(String::length)).values().stream().max(Comparator.comparingInt(List::size)).orElse(Collections.emptyList()).size();
+        int answer = 0;
+        HashMap <Integer, Integer> map = new HashMap <> ();
+
+        for(int i = 0; i < strArr.length; i ++){
+            int key = strArr[i].length();
+
+            if(!map.containsKey(key)){
+                map.put(key, 1);
+            }
+            else{
+                map.put(key, map.get(key) + 1);
+            }
+        }
+
+        //System.out.println(map);
+        int max = 0;
+        for(Map.Entry <Integer, Integer> entry : map.entrySet()){
+            if(entry.getValue() > max){
+                max = entry.getValue();
+            }
+        }
+        //System.out.println(max);
+
+        return max;
     }
 }
