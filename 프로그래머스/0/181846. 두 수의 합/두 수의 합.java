e6cdd1e36538a02f8da4522ed5c0dd15437866b8
InitTester <143479869+InitTester@users.noreply.github.com>
2024-06-01 22:06:43 +0900

    [level 0] Title: 두 수의 합, Time: 223.39 ms, Memory: 95.2 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,14 +1,28 @@
-import java.math.*;
+// import java.math.*;
 
+// class Solution {
+//     public String solution(String a, String b) {
+//         String answer = "";
+        
+//         BigInteger big_a = new BigInteger(a);
+//         BigInteger big_b = new BigInteger(b);
+        
+//         answer = big_a.add(big_b) + "";
+        
+//         return answer;
+//     }
+// }
+
+import java.math.*;
 class Solution {
     public String solution(String a, String b) {
         String answer = "";
-        
-        BigInteger big_a = new BigInteger(a);
-        BigInteger big_b = new BigInteger(b);
-        
-        answer = big_a.add(big_b) + "";
-        
+
+        BigInteger A = new BigInteger(a);
+        BigInteger B = new BigInteger(b);
+
+        answer += A.add(B);
+
         return answer;
     }
 }
