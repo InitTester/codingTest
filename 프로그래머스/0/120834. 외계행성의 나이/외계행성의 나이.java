5844fc7d690dff989cd9ef5b2c4924801543f23f
InitTester <143479869+InitTester@users.noreply.github.com>
2024-06-03 09:15:37 +0900

    [level 0] Title: 외계행성의 나이, Time: 1.95 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,10 +1,19 @@
+// class Solution {
+//     public String solution(int age) {
+//         String answer = "";
+//         char[] list = Integer.valueOf(age).toString().toCharArray();
+        
+//         for(char i : list){ answer += (char)(97+(Integer.valueOf(i)-48));}
+        
+//         return answer;
+//     }
+// }
+
+
+import java.util.stream.Collectors;
+
 class Solution {
     public String solution(int age) {
-        String answer = "";
-        char[] list = Integer.valueOf(age).toString().toCharArray();
-        
-        for(char i : list){ answer += (char)(97+(Integer.valueOf(i)-48));}
-        
-        return answer;
+        return String.valueOf(age).chars().mapToObj(operand -> String.valueOf((char) (49 + operand))).collect(Collectors.joining());
     }
 }
