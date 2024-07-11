8209cc5520edbccebccbdb489622e5068cddf328
InitTester <143479869+InitTester@users.noreply.github.com>
2024-07-11 21:56:12 +0900

    [level 0] Title: 외계어 사전, Time: 4.24 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
+++ 프로그래머스/0/120869. 외계어 사전/외계어 사전.java
@@ -1,15 +1,24 @@
+// class Solution {
+//     public int solution(String[] spell, String[] dic) {
+//         int answer = 2;
+        
+//         for(int i=0; i< dic.length; i++){
+
+//             int cnt = 0;
+//         	for(String s : spell){                
+//                 if(dic[i].contains(s)) cnt++;
+//                 if(cnt == spell.length) answer = 1;                
+//             }
+//         }
+//         return answer;
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(String[] spell, String[] dic) {
-        int answer = 2;
-        
-        for(int i=0; i< dic.length; i++){
-
-            int cnt = 0;
-        	for(String s : spell){                
-                if(dic[i].contains(s)) cnt++;
-                if(cnt == spell.length) answer = 1;                
-            }
-        }
-        return answer;
+        return Arrays.stream(dic).map(s -> s.chars().sorted().mapToObj(i -> String.valueOf((char) i)).collect(Collectors.joining())).collect(Collectors.toList()).contains(Arrays.stream(spell).sorted().collect(Collectors.joining())) ? 1 : 2;
     }
 }
